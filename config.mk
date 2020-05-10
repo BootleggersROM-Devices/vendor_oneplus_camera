12e83466649e6f6279c1eb8cb92b358952e60864
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-05-10 13:20:47 +0000

    Add ARCore

--- config.mk
+++ config.mk
@@ -2,7 +2,8 @@ PRODUCT_PACKAGES += \
     OnePlusCamera \
     OnePlusCameraService \
     OnePlusGallery \
-    GoogleCamera
+    GoogleCamera \
+    ARCore
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
